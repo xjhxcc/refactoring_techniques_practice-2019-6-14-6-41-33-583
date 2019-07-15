1d8957b27c6a142c785c37acda863c97cde1d81a
xjhxcc <2351889342@qq.com>
2019-07-15 21:40:39 +0800

    Refactor:reduce redundant code

--- src/test/java/com/tws/refactoring/extract_variable/BannerRenderTest.java
+++ src/test/java/com/tws/refactoring/extract_variable/BannerRenderTest.java
@@ -2,48 +2,33 @@ package com.tws.refactoring.extract_variable;
 
 import org.junit.Assert;
 import org.junit.Test;
-
-
 public class BannerRenderTest {
+    BannerRender bannerRender = new BannerRender();
     @Test
     public void should_return_IE_on_Mac_when_call_renderBanner_given_platform_is_mac_and_browser_is_IE() {
-        BannerRender bannerRender = new BannerRender();
-        String platform = "MAC";
-        String browser = "IE";
         String result = "IE on Mac?";
-        String actualResult = bannerRender.renderBanner(platform, browser);
+        String actualResult = bannerRender.renderBanner("MAC", "IE");
         Assert.assertSame(result,actualResult);
     }
 
     @Test
     public void should_return_banner_when_call_renderBanner_given_platform_not_is_mac_and_browser_is_IE() {
-        BannerRender bannerRender = new BannerRender();
-        String platform = "Windows";
-        String browser = "IE";
         String result = "banner";
-        String actualResult = bannerRender.renderBanner(platform, browser);
+        String actualResult = bannerRender.renderBanner("Windows", "IE");
         Assert.assertSame(result,actualResult);
     }
 
     @Test
     public void should_return_banner_when_call_renderBanner_given_platform_is_mac_and_browser_not_is_IE() {
-        BannerRender bannerRender = new BannerRender();
-        String platform = "MAC";
-        String browser = "Chrome";
         String result = "banner";
-        String actualResult = bannerRender.renderBanner(platform, browser);
-
+        String actualResult = bannerRender.renderBanner("MAC", "Chrome");
         Assert.assertSame(result,actualResult);
     }
 
     @Test
     public void should_return_banner_when_call_renderBanner_given_platform_not_is_mac_and_browser_not_is_IE() {
-        BannerRender bannerRender = new BannerRender();
-        String platform = "Windows";
-        String browser = "Chrome";
         String result = "banner";
-        String actualResult = bannerRender.renderBanner(platform, browser);
-
+        String actualResult = bannerRender.renderBanner("Windows", "Chrome");
         Assert.assertSame(result,actualResult);
     }
 
