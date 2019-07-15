549f081a5a1671f75f0fdad8a35de82035569d1b
xjhxcc <2351889342@qq.com>
2019-07-15 21:11:27 +0800

    “RefacRefactor:change if condition of BannerRender

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -2,10 +2,8 @@ package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
     String renderBanner(String platform, String browser) {
-        if ((platform.toUpperCase().indexOf("MAC") > -1) &&
-                (browser.toUpperCase().indexOf("IE") > -1)) {
-            return "IE on Mac?";
-        }
-        return "banner";
+        int isPlatformMAC = platform.toUpperCase().indexOf("MAC");
+        int isBrowserIE = browser.toUpperCase().indexOf("IE");
+        return ((isPlatformMAC > -1) && (isBrowserIE > -1)) ? "IE on Mac?" : "banner";
     }
 }
